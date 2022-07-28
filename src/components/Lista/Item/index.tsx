5d2689342c693a546ea4d307d23c00f934711f5d
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-07-28 15:01:25 -0300

    completando tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -8,8 +8,12 @@ interface Props extends ITarefa{
 function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
     return (
         <li
-            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-            onClick={() => selecionaTarefa({
+            className={`
+                ${style.item}
+                ${selecionado ? style.itemSelecionado : ''}
+                ${completado ? style.itemCompletado : ''}
+            `}
+            onClick={() => !completado && selecionaTarefa({
                 tarefa,
                 tempo,
                 selecionado,
@@ -18,6 +22,7 @@ function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: P
             })}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span className={style.concluido} aria-label='tarefa completada'> </span>}
         </li>
     )
 }
